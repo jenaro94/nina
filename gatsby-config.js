5b83a7fa37f3e80dc0bf17dfc2e62d914a92923e
Jenaro Calvino <jen.calvineo@gmail.com>
2020-04-16 13:39:03 -0300

    Changed title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `La Niña Somer`,
+    description: `6 meses.`,
+    author: `@jenaroc`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,8 +28,5 @@ module.exports = {
       },
     },
     `gatsby-plugin-emotion`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
